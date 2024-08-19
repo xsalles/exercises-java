ceac0556f78d095a86a471a70c8353f215eb7270
xsalles <xs.salles@gmail.com>
2024-08-19 16:34:12 -0300

    feat: baskara account.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String [] args) {
+
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Digite o valor em celsius que você deseja transformar em fahrenheit");
 
