bca3f2bf3569726d77b522ec7ecd88e31139751d
xsalles <xs.salles@gmail.com>
2024-08-21 16:48:58 -0300

    feat: showed for the user only value interest.

--- src/taxa.java
+++ src/taxa.java
@@ -7,7 +7,7 @@ public class taxa {
         Scanner sc = new Scanner(System.in);
         DecimalFormat df = new DecimalFormat("00");
         
-        double valor, taxa, parcela;
+        double valor, taxa, parcela, valordataxa;
         int meses;
         
         System.out.println("Digite o valor do produto");
@@ -16,11 +16,12 @@ public class taxa {
         System.out.println("Digite a quantidade de meses de atraso do pagamento");
         meses = sc.nextInt();
 
-        System.out.println("Qual a porcentagem de juros sobre o atraso mensal?");
+        System.out.println("Qual a porcentagem da taxa sobre o atraso mensal?");
         taxa = sc.nextDouble();
 
         parcela = valor + (valor * (taxa/100)*meses);
+        valordataxa = valor * (taxa/100)*meses;
 
-        System.out.println("O valor que você deve pagar sobre o atraso é de R$" + (df.format(parcela)));
+        System.out.println("O valor total que você deve pagar sobre o atraso é de R$" + (df.format(parcela)) + ", mas o valor de juros é R$"  + (df.format(valordataxa)));
     }
 }
