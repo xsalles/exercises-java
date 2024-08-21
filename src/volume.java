bd7f62ea299f792414fd7f444cbbc288f2ca7db6
xsalles <xs.salles@gmail.com>
2024-08-21 14:34:47 -0300

    feat: created two inputs for the user type.

--- src/volume.java
+++ src/volume.java
@@ -1,8 +1,25 @@
 package src;
+import java.util.Scanner;
 
 public class volume {
     public static void main(String[] args) {
-        double volume, pi, raio, altura;
+        Scanner sc = new Scanner(System.in);
         
+        double volume, pi, raio, altura;
+
+        System.out.println("Digite o valor do raio");
+        raio = sc.nextDouble();
+
+        System.out.println("Digite o valor da altura");
+        altura= sc.nextDouble();
+
+        pi = 3.14159;
+
+        volume = (pi*(raio*raio)*altura);
+
+        System.out.println("Resultado: " + volume);
+
+
+
     }
 }
